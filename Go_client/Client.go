69e60a650c1b43076a0410bf2367a878165d59a8
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-08-21 00:36:13 +0300

    _

--- Go_client/Client.go
+++ Go_client/Client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 )
@@ -13,17 +15,70 @@ func Err_check(err error) {
 	}
 }
 
-func main() {
-	user_id := "64dcd4c0aad456d0e90a9f3b"
-	base, err := url.Parse("http://127.0.0.1:5000/user/authentication")
+type User struct {
+	User_id string
+	Tokens  map[string]string
+}
+
+func (u *User) Auth(a_url string) {
+	base, err := url.Parse(a_url)
 	Err_check(err)
 	base.RawQuery = url.Values{
-		"user_id": {user_id},
+		"user_id": {u.User_id},
 	}.Encode()
-	respons, err := http.PostForm(base.String(), nil)
+	response, err := http.PostForm(base.String(), nil)
 	Err_check(err)
-	defer respons.Body.Close()
-	rec_data, err := io.ReadAll(respons.Body)
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&u.Tokens)
 	Err_check(err)
-	fmt.Println(string(rec_data))
+}
+
+func (u *User) Refresh(a_url string) {
+	data := make([]byte, base64.StdEncoding.EncodedLen(len(u.Tokens["refresh_token"])))
+	base64.StdEncoding.Encode(data, []byte(u.Tokens["refresh_token"]))
+	response, err := http.Post(a_url, "application/x-www-form-urlencoded", bytes.NewBuffer(data))
+	Err_check(err)
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&u.Tokens)
+	Err_check(err)
+}
+
+func main() {
+
+	//Rest маршруты
+	urls := map[string]string{
+		"Auth":    "http://127.0.0.1:5000/user/authentication",
+		"Refresh": "http://127.0.0.1:5000/user/refresh",
+	}
+
+	// Пользователь: его id, токены
+	u := User{
+		User_id: "64dcd4c0aad456d0e90a9f3b",
+		Tokens: map[string]string{
+			"access_token":  "Nil",
+			"refresh_token": "Nil",
+		},
+	}
+	for loop := true; loop; {
+		fmt.Print("\033[H\033[2J")
+		fmt.Printf("User id: %s\n", u.User_id)
+		fmt.Printf("Access token: %s\n", u.Tokens["access_token"])
+		fmt.Printf("Refresh token: %s\n\n", u.Tokens["refresh_token"])
+		fmt.Printf("1. Authenticate\n")
+		fmt.Printf("2. Refresh\n")
+		fmt.Printf("3. Exit\n")
+		fmt.Printf("Enter your choice: ")
+		var choice int
+		fmt.Scanf("%d", &choice)
+		switch choice {
+		case 1:
+			u.Auth(urls["Auth"])
+		case 2:
+			u.Refresh(urls["Refresh"])
+		case 3:
+			loop = false
+		default:
+			fmt.Printf("Invalid choice\n")
+		}
+	}
 }
